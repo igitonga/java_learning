7aa1e77342bf26f8cf5f3a8e4fc73a4ac23cf238
davis <hdronoh@gmail.com>
2021-05-20 19:09:13 +0300

    Palindrome String

---
+++ src/Palindrome.java
@@ -0,0 +1,22 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        String input = "ara ara";
+        System.out.println(isPalindrome(input));
+    }
+    public static boolean isPalindrome(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
